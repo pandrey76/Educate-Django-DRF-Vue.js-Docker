59195f7670b36f2b36c833100c5efd7f0b25dbb7
pandrey76 <pandrey76@yandex.ru>
2020-03-30 16:54:31 +0300

    Make first Models in Django project.
    Add "core" to settings in backend project.
    Continue viewing video lesson (Django - DRF - Vue-js - Docker, YOUTUBE User: Роман Китаев).
    Time: 00:27:20.

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    """
+
+    """
+    # Марка производителя
+    vendor = models.CharField(max_length=128)
+    # Модель
+    model = models.CharField(max_length=128)
+    # Год
+    year = models.PositiveSmallIntegerField()
+    #   Гибкость ORM заключается в том , что например в Postgress под тип "PositiveSmallIntegerField" выделится 2 байта,
+    #   а в SQLight 4 байта, но для нас в коде это всё будет выглядеть одинаково. ORM сам решает как рпаботать с той
+    #   или иной СУБД.
+
+    # Объем двигателя.
+    volume = models.PositiveSmallIntegerField()
+
