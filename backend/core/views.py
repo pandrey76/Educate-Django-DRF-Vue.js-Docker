fef0adc0a4f2f7169cadb1ded895c67560f95bf1
pandrey76 <pandrey76@yandex.ru>
2020-03-31 11:58:18 +0300

    Editing urls.py and views.py in "core" application in Django project.
    Continue viewing video lesson (Django - DRF - Vue-js - Docker, YOUTUBE User: Роман Китаев).
    Time: 00:33 :12.

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from backend.core.models import Car
+
+
+def all_cars(request):
+    # Нам надо взять все машины, машины получаются так, у каждой модели Django есть мэнаджер , как минимум один
+    # дефолтгый менаджер "objects". Мэнаджеры это репозитории или контейнеры, которые могут отдавать какие, то объеты
+    # для фильтрации
+    result = []
+    cars = Car.objects.all()
+
+    # Django не даст нам возвратить масив из-за уязвимостей JSON, поэтому мы пишем save=False, говоря тем самым
+    # делвай так.
+    # Здесь нам надо пройти по всем нашим машинам со всеми  их характеристиками, закинуть их в словарь, закинуть это
+    # словарь в result.
+    for car in cars:
+        result.append({
+            "vendor": car.vendor,
+            "model": car.model,
+            "year": car.year,
+            "volume": car.volume,
+        })
+
+    return JsonResponse(result, safe=False)
