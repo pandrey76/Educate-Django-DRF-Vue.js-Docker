cb9cce1a1a0b3397b43246ee0e114d8e79fd8363
pandrey76 <pandrey76@yandex.ru>
2020-07-05 21:15:51 +0300

    Turn-on new Car Serializer.
    Remove old code from "all_cars" function and add getting fields from our models through serializer.
    
    Continue viewing video lesson (Django - DRF - Vue-js - Docker, YOUTUBE User: Роман Китаев).
    Time: 00:56:20.

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
-
 from core.models import Car
-
+from core.serializers import CarSerializer
 
 def all_cars(request):
     # Нам надо взять все машины, машины получаются так, у каждой модели Django есть мэнаджер , как минимум один
@@ -15,11 +14,6 @@ def all_cars(request):
     # Здесь нам надо пройти по всем нашим машинам со всеми  их характеристиками, закинуть их в словарь, закинуть это
     # словарь в result.
     for car in cars:
-        result.append({
-            "vendor": car.vendor,
-            "model": car.model,
-            "year": car.year,
-            "volume": car.volume,
-        })
+        result.append(CarSerializer(car).data)
 
     return JsonResponse(result, safe=False)
